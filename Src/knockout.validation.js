e0c67525f41f3d5b5b5bf9486d9ea3019c7af105
ericmbarnard <eric.m.barnard@gmail.com>
2012-07-01 21:06:18 -0500

    issue #80 - pattern html5 validation

--- Src/knockout.validation.js
+++ Src/knockout.validation.js
@@ -407,17 +407,26 @@
 
                 // loop through the attributes and add the information needed
                 ko.utils.arrayForEach(html5Attributes, function (attr) {
-
-                    var ctx = ko.utils.arrayFirst(contexts, function(ctx){
+                    var params;
+                    var ctx = ko.utils.arrayFirst(contexts, function (ctx) {
                         return ctx.rule.toLowerCase() === attr.toLowerCase();
                     });
 
                     if (!ctx)
                         return;
 
+                    params = ctx.params;
+
+                    // we have to do some special things for the pattern validation
+                    if (ctx.rule == "pattern") {
+                        if (ctx.params instanceof RegExp) {
+                            params = ctx.params.source; // we need the pure string representation of the RegExpr without the //gi stuff
+                        }
+                    }
+
                     // we have a rule matching a validation attribute at this point
                     // so lets add it to the element along with the params
-                    $el.attr(attr, ctx.params);
+                    $el.attr(attr, params);
                 });
 
                 contexts = null;
