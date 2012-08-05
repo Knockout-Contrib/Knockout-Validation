a54ad5038589237fe0d64dc8e43e5d710e46df85
Terence Lewis <lewistm@gmail.com>
2012-08-05 18:43:13 +0200

    Bugfix for isValidatable with undefined parameter

--- Src/knockout.validation.js
+++ Src/knockout.validation.js
@@ -67,7 +67,7 @@
                 return node.getAttribute(attr) !== null;
             },
             isValidatable: function (o) {
-                return o.rules && o.isValid && o.isModified;
+                return o && o.rules && o.isValid && o.isModified;
             },
             insertAfter: function (node, newNode) {
                 node.parentNode.insertBefore(newNode, node.nextSibling);
