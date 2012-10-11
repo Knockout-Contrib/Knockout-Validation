8aa1c00971dd2ce3e3ae242a51e9415a5f58bb71
IanYates83 <ian@iyates.co.uk>
2012-10-11 23:23:19 +0100

    One more test to cover other non-string types

--- Tests/validation-tests.js
+++ Tests/validation-tests.js
@@ -283,6 +283,36 @@ test('Pattern validation mismatches numbers', function () {
     equal(testObj.isValid(), false, 'testObj is not valid');
 });
 
+test('Pattern validation doesn\'t break with non-string values', function () {
+    var testObj = ko.observable('')
+                    .extend({ pattern: '^$' });
+	
+	// Validation results not important, just shouldn't blow-up
+    testObj(null);
+	testObj.isValid();
+
+    testObj(undefined);
+	testObj.isValid();
+
+    testObj(12345);
+	testObj.isValid();
+
+    testObj(12.34);
+	testObj.isValid();
+
+    testObj(true);
+	testObj.isValid();
+
+    testObj(false);
+	testObj.isValid();
+
+    testObj([]);
+	testObj.isValid();
+
+    testObj({});
+	testObj.isValid();
+});
+
 //#endregion
 
 //#region Step Validation
