4b7d9e4971b30c076117402686a49a5517680eb7
Steve Greatrex <stephen.greatrex@googlemail.com>
2013-10-18 21:19:36 +0100

    Fixed whitespace issues

--- Tests/validation-tests.js
+++ Tests/validation-tests.js
@@ -1366,81 +1366,81 @@ asyncTest('Async Rule Is NOT Valid Test', function () {
 module("setRules Tests");
 
 test("setRules applies rules to all properties", function () {
-	    var definition = {
-		        property1: {
-			            required: true,
-			            min: 10,
-			            max: 99,
-			            ignoredDefinition: { required: true }
-			        },
-			        child: {
-				            property2: {
-					                pattern: {
-						                    params: "^[a-z0-9].$",
-						                    message: "Only AlphaNumeric please"
-						                }
-					            },
-					            grandchild: {
-						                property3: {
-							                    number: true
-							                }
-						            },
-						            ignoredDefinition: { required: true }
-				        },
-				        nestedArray: {
-					            property4: { email: true },
-					            ignoredDefinition: { required: true }
-					        }
-	    };
+	var definition = {
+		property1: {
+			required: true,
+			min: 10,
+			max: 99,
+			ignoredDefinition: { required: true }
+		},
+		child: {
+			property2: {
+				pattern: {
+					params: "^[a-z0-9].$",
+					message: "Only AlphaNumeric please"
+				}
+			},
+			grandchild: {
+				property3: {
+					number: true
+				}
+			},
+			ignoredDefinition: { required: true }
+		},
+		nestedArray: {
+			property4: { email: true },
+			ignoredDefinition: { required: true }
+		}
+	};
 
-    var target = {
-	        property1: ko.observable(),
-	        ignoredProperty: ko.observable(),
-	        child: {
-	            property2: ko.observable(),
-	            ignoredProperty: ko.observable(),
-	            grandchild: {
-		                property3: ko.observable(),
-		                ignoredProperty: ko.observable(),
-		            }
-	        },
-        nestedArray: ko.observableArray([
+	var target = {
+		property1: ko.observable(),
+		ignoredProperty: ko.observable(),
+		child: {
+			property2: ko.observable(),
+			ignoredProperty: ko.observable(),
+			grandchild: {
+				property3: ko.observable(),
+				ignoredProperty: ko.observable(),
+			}
+		},
+		nestedArray: ko.observableArray([
             { property4: ko.observable(), ignoredProperty: ko.observable() },
             { property4: ko.observable(), ignoredProperty: ko.observable() },
             { property4: ko.observable(), ignoredProperty: ko.observable() }
-        ])
-    };
+		])
+	};
 
-    ko.validation.setRules(target, definition);
+	ko.validation.setRules(target, definition);
 
-    //check that all rules have been applied
-    deepEqual(target.property1.rules(), [
+	//check that all rules have been applied
+	deepEqual(target.property1.rules(), [
         { rule: "required", params: true },
         { rule: "min", params: 10 },
         { rule: "max", params: 99 }
-    ]);
+	]);
 
-    deepEqual(target.child.property2.rules(), [
+	deepEqual(target.child.property2.rules(), [
         { rule: "pattern", message: "Only AlphaNumeric please", params: "^[a-z0-9].$", condition: undefined }
-    ]);
+	]);
 
-    deepEqual(target.child.grandchild.property3.rules(), [
+	deepEqual(target.child.grandchild.property3.rules(), [
         { rule: "number", params: true }
-    ]);
+	]);
 
-    for (var i = 0; i < target.nestedArray.length; i) {
-	        deepEqual(target.nestedArray[i].property3.rules(), [
-	            { rule: "email", params: true }
-	        ]);
-	    }
+	for (var i = 0; i < target.nestedArray.length; i) {
+		deepEqual(target.nestedArray[i].property3.rules(), [
+			{ rule: "email", params: true }
+		]);
+	}
 
-    //check that ignored properties have not had rules added
-    ok(!target.ignoredProperty.rules);
-    ok(!target.child.ignoredProperty.rules);
-    ok(!target.child.grandchild.ignoredProperty.rules);
-    ok(!target.nestedArray()[0].ignoredProperty.rules);
-    ok(!target.nestedArray()[1].ignoredProperty.rules);
-    ok(!target.nestedArray()[2].ignoredProperty.rules);
+	//check that ignored properties have not had rules added
+	ok(!target.ignoredProperty.rules);
+	ok(!target.child.ignoredProperty.rules);
+	ok(!target.child.grandchild.ignoredProperty.rules);
+	ok(!target.nestedArray()[0].ignoredProperty.rules);
+	ok(!target.nestedArray()[1].ignoredProperty.rules);
+	ok(!target.nestedArray()[2].ignoredProperty.rules);
 });
 
 //#endregion
