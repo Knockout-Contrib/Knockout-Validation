f91ab3cae5e402e134d19786a2b7e93851d72484
emilast <emil_astrom@hotmail.com>
2014-04-24 07:13:02 +0200

    Update sv-SE.js
    Fixed a spelling error in the Swedish translation of the error message for required fields.

--- Localization/sv-SE.js
+++ Localization/sv-SE.js
@@ -20,7 +20,7 @@
 */
 
 ko.validation.localize({
-    required: 'Detta fält är obligatorisk',
+    required: 'Detta fält är obligatoriskt',
     min: 'Fyll i ett värde som är större än eller lika med {0}',
     max: 'Fyll i ett värde som är mindre än eller lika med {0}',
     minLength: 'Fyll i minst {0} tecken',
