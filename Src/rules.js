36236af63bb8fb7c3e6e3dc3d6df840be561c034
geeksnglitter <geeksnglitter@gmail.com>
2014-08-14 14:23:36 -0400

    Add punctuation to validation messages to increase consistency

--- Src/rules.js
+++ Src/rules.js
@@ -177,7 +177,7 @@ ko.validation.rules['step'] = {
 		var dif = (val * 100) % (step * 100);
 		return Math.abs(dif) < 0.00001 || Math.abs(1 - dif) < 0.00001;
 	},
-	message: 'The value must increment by {0}'
+	message: 'The value must increment by {0}.'
 };
 
 ko.validation.rules['email'] = {
@@ -191,7 +191,7 @@ ko.validation.rules['email'] = {
 			validate && /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i.test(val)
 		);
 	},
-	message: 'Please enter a proper email address'
+	message: 'Please enter a proper email address.'
 };
 
 ko.validation.rules['date'] = {
@@ -199,7 +199,7 @@ ko.validation.rules['date'] = {
 		if (!validate) { return true; }
 		return ko.validation.utils.isEmptyVal(value) || (validate && !/Invalid|NaN/.test(new Date(value)));
 	},
-	message: 'Please enter a proper date'
+	message: 'Please enter a proper date.'
 };
 
 ko.validation.rules['dateISO'] = {
@@ -207,7 +207,7 @@ ko.validation.rules['dateISO'] = {
 		if (!validate) { return true; }
 		return ko.validation.utils.isEmptyVal(value) || (validate && /^\d{4}[\/\-]\d{1,2}[\/\-]\d{1,2}$/.test(value));
 	},
-	message: 'Please enter a proper date'
+	message: 'Please enter a proper date.'
 };
 
 ko.validation.rules['number'] = {
@@ -215,7 +215,7 @@ ko.validation.rules['number'] = {
 		if (!validate) { return true; }
 		return ko.validation.utils.isEmptyVal(value) || (validate && /^-?(?:\d+|\d{1,3}(?:,\d{3})+)?(?:\.\d+)?$/.test(value));
 	},
-	message: 'Please enter a number'
+	message: 'Please enter a number.'
 };
 
 ko.validation.rules['digit'] = {
@@ -223,7 +223,7 @@ ko.validation.rules['digit'] = {
 		if (!validate) { return true; }
 		return ko.validation.utils.isEmptyVal(value) || (validate && /^\d+$/.test(value));
 	},
-	message: 'Please enter a digit'
+	message: 'Please enter a digit.'
 };
 
 ko.validation.rules['phoneUS'] = {
@@ -234,7 +234,7 @@ ko.validation.rules['phoneUS'] = {
 		phoneNumber = phoneNumber.replace(/\s+/g, "");
 		return validate && phoneNumber.length > 9 && phoneNumber.match(/^(1-?)?(\([2-9]\d{2}\)|[2-9]\d{2})-?[2-9]\d{2}-?\d{4}$/);
 	},
-	message: 'Please specify a valid phone number'
+	message: 'Please specify a valid phone number.'
 };
 
 ko.validation.rules['equal'] = {
@@ -242,7 +242,7 @@ ko.validation.rules['equal'] = {
 		var otherValue = params;
 		return val === ko.validation.utils.getValue(otherValue);
 	},
-	message: 'Values must equal'
+	message: 'Values must equal.'
 };
 
 ko.validation.rules['notEqual'] = {
