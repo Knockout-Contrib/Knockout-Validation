e3d32f567b6b3b237a6aee23d6a90f2ab88d4c55
davhdavh <davh@davh.dk>
2014-12-18 12:08:06 +0700

    Make regexp html5 pattern work when observable

--- Src/api.js
+++ Src/api.js
@@ -360,7 +360,6 @@
 
 			// loop through the attributes and add the information needed
 			ko.utils.arrayForEach(ko.validation.configuration.html5Attributes, function (attr) {
-				var params;
 				var ctx = ko.utils.arrayFirst(contexts, function (ctx) {
 					return ctx.rule && ctx.rule.toLowerCase() === attr.toLowerCase();
 				});
@@ -369,19 +368,20 @@
 					return;
 				}
 
-				params = ctx.params;
-
-				// we have to do some special things for the pattern validation
-				if (ctx.rule === "pattern") {
-					if (ctx.params instanceof RegExp) {
-						params = ctx.params.source; // we need the pure string representation of the RegExpr without the //gi stuff
-					}
-				}
-
 				// we have a rule matching a validation attribute at this point
 				// so lets add it to the element along with the params
 				ko.computed({
-					read: function() { element.setAttribute(attr, ko.unwrap(params)); },
+					read: function() {
+						var params = ko.unwrap(ctx.params);
+
+						// we have to do some special things for the pattern validation
+						if (ctx.rule === "pattern" && params instanceof RegExp) {
+							// we need the pure string representation of the RegExpr without the //gi stuff
+							params = params.source; 
+						}
+
+						element.setAttribute(attr, params); 
+					},
 					disposeWhenNodeIsRemoved: element,
 				});
 			});
