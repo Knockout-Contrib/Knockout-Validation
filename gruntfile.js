86bf60dec3959505b16f07f769e173731f3547d7
crissdev <criss.trifan@gmail.com>
2015-03-01 14:31:20 +0200

    Create localization bundle
    Localization files will now be distributed in three ways:
    
    - included in the library itself (dist/knockout-validation-with-locales.js)
    - bundle with all localization files (dist/locales.js)
    - individual files in localization directory (localization/*.js)
    
    Closes #541

--- gruntfile.js
+++ gruntfile.js
@@ -2,10 +2,54 @@
 /*jshint node:true*/
 module.exports = function (grunt) {
 
+
+	function processFileSource(contents, filePath, embedLocales) {
+		if (embedLocales && filePath.indexOf('localization/') === 0) {
+			var re = /function\(factory\) \{[^]*}(?=\(function\(kv\) \{)/gm;
+			var replacement = 'function(factory) {\n    factory(ko.validation);\n}';
+
+			if (!re.test(contents)) {
+				grunt.log.warn('Localization file cannot be embedded: ', filePath);
+				return '/*Failed to embed localization file: ' + filePath + '*/\n';
+			}
+			else {
+				contents = contents.replace(re, replacement);
+			}
+		}
+		return contents
+			.replace(/ko\.validation\./g, 'kv.')
+			.replace(/ko\.utils\.arrayForEach/g, 'forEach')
+			.replace(/ko\.utils\.extend/g, 'extend')
+			.replace(/ko\.utils\.unwrapObservable/g, 'unwrap')
+			.replace(/ko\.utils\./g, 'koUtils.');
+	}
+
+	function getSourceFiles(includeLocales) {
+		var items = [
+			"<%= meta.banner %>",
+			"src/ko.validation.start.frag",
+			"src/configuration.js",
+			"src/utils.js",
+			"src/api.js",
+			"src/rules.js",
+			"src/bindingHandlers.js",
+			"src/extenders.js",
+			"src/localization.js",
+			"src/ko.extensions.js"
+		];
+		if (includeLocales) {
+			items.push(["localization/*.js", "!localization/en-US.js"]);
+		}
+		items.push("src/ko.validation.end.frag");
+		return items;
+	}
+
+
 	// Project configuration.
 	grunt.initConfig({
 		pkg: grunt.file.readJSON("package.json"),
 		meta: {
+			banner_l10n: "/* Localization for Knockout Validation <%= pkg.version %> */",
 			banner: "/*=============================================================================\n" +
 					"	Author:			Eric M. Barnard - @ericmbarnard								\n" +
 					"	License:		MIT (http://opensource.org/licenses/mit-license.php)		\n" +
@@ -18,30 +62,30 @@ module.exports = function (grunt) {
 			options: {
 				separator: ";",
 				banner: "<%= meta.banner %>",
-				process: function (src) {
-					return src
-						.replace(/ko\.validation\./g, 'kv.')
-						.replace(/ko\.utils\.arrayForEach/g, 'forEach')
-						.replace(/ko\.utils\.extend/g, 'extend')
-						.replace(/ko\.utils\.unwrapObservable/g, 'unwrap')
-						.replace(/ko\.utils\./g, 'koUtils.');
+				process: function(contents, filePath) {
+					return processFileSource(contents, filePath, false);
 				}
 			},
 			dist: {
-				src: [
-					"<%= meta.banner %>",
-					"src/ko.validation.start.frag",
-					"src/configuration.js",
-					"src/utils.js",
-					"src/api.js",
-					"src/rules.js",
-					"src/bindingHandlers.js",
-					"src/extenders.js",
-					"src/localization.js",
-					"src/ko.extensions.js",
-					"src/ko.validation.end.frag"
-				],
+				src: getSourceFiles(false),
 				dest: "dist/<%= pkg.name %>.js"
+			},
+			dist_locales: {
+				options: {
+					process: function(contents, filePath) {
+						return processFileSource(contents, filePath, true);
+					}
+				},
+				src: getSourceFiles(true),
+				dest: "dist/<%= pkg.name %>-with-locales.js"
+			},
+			locales: {
+				options: {
+					process: null,
+					banner: "<%= meta.banner_l10n %>\n\n"
+				},
+				src: ['localization/*.js', '!localization/en-US.js'],
+				dest: "dist/locales.js"
 			}
 		},
 		uglify: {
@@ -53,6 +97,19 @@ module.exports = function (grunt) {
 			dist: {
 				files: {
 					"dist/<%= pkg.name %>.min.js": ["<%= concat.dist.dest %>"]
+				}
+			},
+			dist_locales: {
+				files: {
+					"dist/<%= pkg.name %>-with-locales.min.js": ["<%= concat.dist_locales.dest %>"]
+				}
+			},
+			locales: {
+				options: {
+					banner: "<%= meta.banner_l10n %>"
+				},
+				files: {
+					"dist/locales.min.js": ["<%= concat.locales.dest %>"]
 				}
 			}
 		},
