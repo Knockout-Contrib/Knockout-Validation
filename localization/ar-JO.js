249efc1844b4425226db11c7fccd893920cfe7c2
crissdev <criss.trifan@gmail.com>
2015-03-01 14:31:20 +0200

    Create localization bundle
    Localization files will now be distributed in three ways:
    
    - included in the library itself (dist/knockout-validation-with-locales.js)
    - bundle with all localization files (dist/locales.js)
    - individual files in localization directory (localization/*.js)
    
    Closes #541

--- localization/ar-JO.js
+++ localization/ar-JO.js
@@ -2,22 +2,15 @@
  * Localization file for Arabic - Jordan (ar-JO)
  */
 (function(factory) {
-	// Module systems magic dance.
-	/*global require,ko.validation,define,module*/
-	if (typeof require === 'function' && typeof exports === 'object' && typeof module === 'object') {
-		// CommonJS or Node
-		module.exports = factory(require('../'));
-	} else if (typeof define === 'function' && define['amd']) {
-		// AMD anonymous module with hard-coded dependency on 'knockout.validation'
-		define(['knockout.validation'], factory);
-	} else {
-		// <script> tag: use the global `ko.validation` object
-		factory(ko.validation);
-	}
+    /*global ko,require,define,module*/
+    if (typeof require === 'function' && typeof exports === 'object' && typeof module === 'object') {
+        module.exports = factory(require('../'));
+    } else if (typeof define === 'function' && define.amd) {
+        define(['knockout.validation'], factory);
+    } else {
+        factory(ko.validation);
+    }
 }(function(kv) {
-	if (!kv || typeof kv.defineLocale !== 'function') {
-		throw new Error('Knockout-Validation is required, please ensure it is loaded before this localization file');
-	}
     return kv.defineLocale('ar-JO', {
         required: 'مطلوب.',
         min: 'أدخل قيمة اكبر من {0}.',
--- localization/en-US.js
+++ localization/en-US.js
@@ -20,39 +20,35 @@
 * such as numbers and strings. We do not stringify complex objects
 * or anything like that currently.
 */
+/**
+ * Localization file for English - United States (en-US)
+ */
 (function(factory) {
-	// Module systems magic dance.
-	/*global require,ko.validation,define,module*/
-	if (typeof require === 'function' && typeof exports === 'object' && typeof module === 'object') {
-		// CommonJS or Node
+    /*global ko,require,define,module*/
+    if (typeof require === 'function' && typeof exports === 'object' && typeof module === 'object') {
         module.exports = factory(require('../'));
-	} else if (typeof define === 'function' && define['amd']) {
-		// AMD anonymous module
-		define(['knockout.validation'], factory);
-	} else {
-		// <script> tag: use the global `ko.validation` object
-		factory(ko.validation);
-	}
+    } else if (typeof define === 'function' && define.amd) {
+        define(['knockout.validation'], factory);
+    } else {
+        factory(ko.validation);
+    }
 }(function(kv) {
-	if (!kv || typeof kv.defineLocale !== 'function') {
-		throw new Error('Knockout-Validation is required, please ensure it is loaded before this localization file');
-	}
-	return kv.defineLocale('en-US', {
-		required: 'This field is required.',
-		min: 'Please enter a value greater than or equal to {0}.',
-		max: 'Please enter a value less than or equal to {0}.',
-		minLength: 'Please enter at least {0} characters.',
-		maxLength: 'Please enter no more than {0} characters.',
-		pattern: 'Please check this value.',
-		step: 'The value must increment by {0}.',
-		email: 'Please enter a proper email address.',
-		date: 'Please enter a proper date.',
-		dateISO: 'Please enter a proper date.',
-		number: 'Please enter a number.',
-		digit: 'Please enter a digit.',
-		phoneUS: 'Please specify a valid phone number.',
-		equal: 'Values must equal.',
-		notEqual: 'Please choose another value.',
-		unique: 'Please make sure the value is unique.'
-	});
+    return kv.defineLocale('en-US', {
+        required: 'This field is required.',
+        min: 'Please enter a value greater than or equal to {0}.',
+        max: 'Please enter a value less than or equal to {0}.',
+        minLength: 'Please enter at least {0} characters.',
+        maxLength: 'Please enter no more than {0} characters.',
+        pattern: 'Please check this value.',
+        step: 'The value must increment by {0}.',
+        email: 'Please enter a proper email address.',
+        date: 'Please enter a proper date.',
+        dateISO: 'Please enter a proper date.',
+        number: 'Please enter a number.',
+        digit: 'Please enter a digit.',
+        phoneUS: 'Please specify a valid phone number.',
+        equal: 'Values must equal.',
+        notEqual: 'Please choose another value.',
+        unique: 'Please make sure the value is unique.'
+    });
 }));
