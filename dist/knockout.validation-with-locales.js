05c23000bcdead1f075a945cb77e50e71e8874ee
harrison314 <harrison314@azet.sk>
2016-02-06 11:12:45 +0100

    Add Slovak - Slovak Republic (sk-SK) localization
    Closes #605

--- dist/knockout.validation-with-locales.js
+++ dist/knockout.validation-with-locales.js
@@ -2141,6 +2141,31 @@ ko.validatedObservable = function (initialValue, options) {
     });
 }));
 ;/**
+ * Localization file for Slovak - Slovak Republic (sk-SK)
+ */
+(function(factory) {
+    factory(ko.validation);
+}(function(kv) {
+    return kv.defineLocale('sk-SK', {
+        required: 'Toto pole je povinné.',
+        min: 'Zadajte číslo väčšie alebo rovné {0}.',
+        max: 'Zadajte  číslo menšie alebo rovné {0}.',
+        minLength: 'Vložte minimálne {0} znakov.',
+        maxLength: 'Vložte najviac {0} znakov.',
+        pattern: 'Skontrolujte formát poľa.',
+        step: 'Hodnota musí byť násobok {0}.',
+        email: 'Neplatná e-mailová adresa.',
+        date: 'Zadajte platný dátum.',
+        dateISO: 'Zadajte platný dátum.',
+        number: 'Zadajte číslo.',
+        digit: 'Zadajte číslice.',
+        phoneUS: 'Zadajte platné telefónne číslo.',
+        equal: 'Hodnoty musia byť rovnaké.',
+        notEqual: 'Vyberte inú hodnotu.',
+        unique: 'Skontrolujte, či je zadaná hodnota jedinečná.'
+    });
+}));
+;/**
  * Localization file for Swedish - Sweden (sv-SE)
  */
 (function(factory) {
